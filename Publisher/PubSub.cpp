986af18c0288e55546d5ce934220f28b0d7414df
Harshvardhan R <harsh.aayush.r1@gmail.com>
2019-11-14 22:42:56 +0530

    Work

--- Publisher/PubSub.cpp
+++ Publisher/PubSub.cpp
@@ -14,11 +14,11 @@
 #define PORT_PUB_SUB 7000
 
 #define SERV_IP "127.0.0.1"
-std::string KEY="123456";
+std::string KEY="123456\n";
 
 using namespace std;
 
-class PublisherConnection
+class PubSub
 {
 public:
 	struct sockaddr_in ALSServAddr;
@@ -28,11 +28,7 @@ public:
 	socklen_t SubLen;
 	struct sockaddr_in ALCServAddr, Sub_Addr;
 
-	string receive(int);
-	void connectToServer(); 
 	void listenForSubscriber(); 
-	void serverShowsList();
-	void sendCategoryAndFile();
 	void askForFile();
 	void sendFileToSub(const char *file);
 	void error(const char *msg)
@@ -42,99 +38,9 @@ public:
 	}
 };
 static long key = 123456;
-string PublisherConnection::receive(int socket)
+void PubSub::listenForSubscriber()
 {
-	char buffer[1024];
-	int bytesRead;
-	string receivedData;
-	do
-	{
-		bytesRead = recv(socket, buffer, 1024 - 2, 0);
-		buffer[1024 - 1] = '\0';
-		receivedData += buffer;
-	} while (bytesRead != 0);
 
-	return receivedData;
-}
-void PublisherConnection::connectToServer()
-{
-	//only runs for first time connection and only then
-	ALCSockFd = socket(AF_INET, SOCK_STREAM, 0);
-	if (ALCSockFd < 0)
-	{
-		error("ERROR opening socket");
-	}
-	bzero(&ALCServAddr, sizeof(ALCServAddr));
-	ALCServAddr.sin_family = AF_INET;
-	ALCServAddr.sin_addr.s_addr = INADDR_ANY;
-	ALCServAddr.sin_port = htons(SERV_PORT);
-	inet_pton(AF_INET, SERV_IP, &ALCServAddr.sin_addr);
-	if (connect(ALCSockFd, (struct sockaddr *)&ALCServAddr, sizeof(ALCServAddr)) < 0)
-	{
-		error("ERROR connecting");
-	}
-	cout << "Connected to server"<<endl;
-	recv(ALCSockFd, buffer, 6, 0);
-	cout<<buffer<<endl;
-	write(ALCSockFd,KEY.c_str(),sizeof(KEY));
-	int ch;
-	do{
-		cout<<"Do you want to see the list of categories(1) or send new file(2)  0 for exit?"<<endl;
-		cin>>ch;
-		switch(ch)
-		{
-		case 1:{
-			send(ALCSockFd, "1", 2, 0);
-			cout<<"Displaying list"<<endl;
-			serverShowsList();
-			break;
-		}
-		case 2:{
-			send(ALCSockFd, "2", 2, 0);
-			cout<<"Enter the category"<<endl;
-			sendCategoryAndFile();
-			break;
-		}
-		case 0:{
-			cout<<"Closing socket and exiting"<<endl;
-			close(ALCSockFd);
-			exit(0);
-			break;
-		}
-		default:
-			cout<<"Invalid choice!"<<endl;
-			break;
-		}
-	}while(ch!=0);
-}
-
-void PublisherConnection::serverShowsList()
-{
-	
-	bzero(buffer, 256);
-	if ( read(ALCSockFd, buffer, 256 ) < 0){
-		error("ERROR reading from socket\n");
-		exit(0);
-	}
-	cout<<buffer<<endl;
-}
-void PublisherConnection::sendCategoryAndFile()
-{
-	string category, fileName;
-	int n;
-	cout << "Enter Category\n";
-	cin >> category;
-	cout << "Enter File name\n";
-	cin >> fileName;
-	string buf = category + ":" + fileName;
-	n = write(ALCSockFd, buf.c_str(), buf.length()); //Hardcoded length
-	if (n < 0)
-		error("Error writing to socket\n");
-	bzero(buffer, sizeof(buffer));
-	recv(ALCSockFd, buffer, sizeof(buffer), 0);
-}
-void PublisherConnection::listenForSubscriber()
-{
 	ALSSockFd = socket(AF_INET, SOCK_STREAM, 0);
 	if (ALSSockFd < 0)
 		error("ERROR opening socket\n");
@@ -167,7 +73,8 @@ void PublisherConnection::listenForSubscriber()
 		cout << "Connected to Subscriber\n";
 	askForFile();
 }
-void PublisherConnection::askForFile()
+
+void PubSub::askForFile()
 {
 	char buff[1024];
 	int n, i;
@@ -189,7 +96,7 @@ void PublisherConnection::askForFile()
 		//receving file name
 		bzero(buff, sizeof(buff));
 		read(ALSNewSockFd, buff, sizeof(buff));
-		cout << "Publisher asked for: " << buff<<endl;
+		cout << "Publisher asked for: " << buff<<"\n";
 		sendFileToSub(buff);
 		//printf("From Subscriber : %s", buff);
 		//}
@@ -200,7 +107,7 @@ void PublisherConnection::askForFile()
 		askForFile();
 	}
 }
-void PublisherConnection::sendFileToSub(const char *fileName)
+void PubSub::sendFileToSub(const char *fileName)
 {
 	FILE *f;
 	int words = 0;
@@ -226,6 +133,6 @@ void PublisherConnection::sendFileToSub(const char *fileName)
 
 int main(int argc, char *argv[])
 {
-	PublisherConnection *withSubscriber = new PublisherConnection();
+	PubSub *withSubscriber = new PubSub();
 	withSubscriber->listenForSubscriber();
 }
